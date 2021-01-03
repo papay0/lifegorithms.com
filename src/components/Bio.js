4a643bec43b5203b48b69997d2f600b444a06216
Arthur Papailhau <papailhau@uber.com>
2021-01-03 17:46:33 +0100

    Adding WIP about me section in Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,28 +5,43 @@ import { rhythm } from '../utils/typography';
 class Bio extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2),
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`Arthur Papailhau`}
+      <div>
+        <div
           style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-            borderRadius: '50%',
+            display: 'flex',
+            marginBottom: rhythm(2),
           }}
-        />
-        <p style={{ maxWidth: 310 }}>
-          Personal blog by{' '}
-          <a href="https://mobile.twitter.com/papay0">Arthur Papailhau</a>.{' '}
-          Software Engineer @Uber (Eats)
-        </p>
+        >
+          <img
+            src={profilePic}
+            alt={`Arthur Papailhau`}
+            style={{
+              marginRight: rhythm(1 / 2),
+              marginBottom: 0,
+              width: rhythm(2),
+              height: rhythm(2),
+              borderRadius: '50%',
+            }}
+          />
+          <p style={{ maxWidth: 310 }}>
+            Personal blog by{' '}
+            <a href="https://mobile.twitter.com/papay0">Arthur Papailhau</a>.{' '}
+            Software Engineer @Uber (Eats)
+          </p>
+        </div>
+        <div>
+          <h3
+            style={{
+              fontFamily: 'Montserrat, sans-serif',
+              fontSize: rhythm(1),
+              marginBottom: rhythm(1 / 4),
+              marginTop: -50
+            }}
+          >
+            About me
+          </h3>
+          This is more about me
+        </div>
       </div>
     );
   }
